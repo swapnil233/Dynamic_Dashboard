8ac949f6b0c084c03ab6d4e215f4c75a493eaf58
Hasan Iqbal <36313876+swapnil233@users.noreply.github.com>
2021-06-28 15:08:55 -0400

    Started user's dropdown menu in the dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -16,9 +16,31 @@ firebase.auth().onAuthStateChanged(function (user) {
         // Don't use the uid for varification, use user.getToken() instead
         const uid = user.uid;
 
+        // Display their name
         document.getElementById("user_name").innerHTML = userDisplayName
-
         console.log(userDisplayName)
+
+        // Dropdown Menu
+        document.getElementById('dropdown-icon-wrapper').addEventListener("click", (e) => {
+            e.preventDefault()
+            console.log("SD")
+
+            document.getElementById("myDropdown").classList.toggle("show");
+        })
+
+        // Close the dropdown menu if the user clicks outside of it
+        window.onclick = function (event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
 
     } else {
         window.location.replace('index.html')
@@ -31,7 +53,7 @@ document.getElementById('logout_btn').addEventListener("click", (e) => {
     firebase.auth().signOut().then(() => {
 
         // Will use this async method for later tasks
-        
+
         return
     })
 })
