269eab8318cb4e1b81b81e6471409265e7bf051d
Hasan Iqbal <swapniliqbal@gmail.com>
2021-06-29 19:49:02 -0400

    users can now update their username/display name by using the Update Profile feature from the navbar

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,33 +1,29 @@
-// Get data
-db.collection('guides').get().then(snapshot => {
-    console.log(snapshot.docs)
-})
-
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
 
-        // Fetch user's data for later
-        const userDisplayName = user.displayName;
-        const email = user.email;
-        const photoURL = user.photoURL;
-        const emailVerified = user.emailVerified;
+        // Display user name
+        updateDisplayNameInDOM()
 
-        // Don't use the uid for varification, use user.getToken() instead
-        const uid = user.uid;
+        // Dropdown Nav Menu
+        dropDownMenu()
 
-        // Display their name
-        document.getElementById("user_name").innerHTML = userDisplayName
-        console.log(userDisplayName)
+        // Modal
+        updateProfileModal();
 
     } else {
         window.location.replace('index.html')
     }
 });
 
+// Database
+db.collection('guides').get().then(snapshot => {
+    console.log(snapshot.docs)
+})
+
 // Logout
 document.getElementById('logout_btn').addEventListener("click", (e) => {
     e.preventDefault();
-    firebase.auth().signOut().then(() => {
+    auth.signOut().then(() => {
 
         // Will use this async method for later tasks
 
@@ -35,8 +31,69 @@ document.getElementById('logout_btn').addEventListener("click", (e) => {
     })
 })
 
-// Dropdown menu
-document.querySelector("#profile-dropdown").addEventListener("click", (e) => {
-    // Hide/show the dropdown menu when arrow is clicked
-    document.querySelector(".dropdown-content").classList.toggle("show");
-})
+// Fetch user's data
+const fethUserData = (user) => {
+    const userData = {
+        userDisplayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        emailVerified: user.emailVerified,
+        uid: user.uid
+    };
+    return userData;
+};
+
+const dropDownMenu = () => {
+    document.querySelector("#profile-dropdown").addEventListener("click", (e) => {
+        // Hide/show the dropdown menu when arrow is clicked
+        document.querySelector(".dropdown-content").classList.toggle("show");
+    });
+}
+
+const updateProfileModal = () => {
+    const modal = document.querySelector(".profile-update-wrapper");
+
+    // Open profile updating modal from navbar upon click
+    document.querySelector("#update-profile-btn").addEventListener("click", (e) => {
+        
+        // If the modal doesn't have the CSS "show" class:
+        if (!modal.classList.contains("show")) {
+            
+            // Toggle that class into the modal
+            modal.classList.toggle("show");
+
+            // Upon clicking on "update" button inside modal:
+            document.querySelector("#update").addEventListener("click", (e) => {
+                e.preventDefault();
+
+                // Get value of new displayName
+                const newName = document.querySelector("#profile_name").value;
+
+                // Update displayName.
+                if (newName !== "") {
+                    updateUsername(newName)
+                    updateDisplayNameInDOM()
+                } else {
+                    window.alert("Must choose a new name to update")
+                }
+            })
+        }
+    })
+
+    // Close profile updating modal upon clicking the "x"
+    document.querySelector("#close-modal").addEventListener("click", (e) => {
+        modal.classList.toggle("show");
+    })
+}
+
+// Update user's display name
+const updateUsername = (newName) => {
+    auth.currentUser.updateProfile({
+        displayName: newName
+    });
+}
+
+// Show user's display name inside the DOM
+const updateDisplayNameInDOM = () => {
+    document.querySelector("#user_name").innerHTML = fethUserData(firebase.auth().currentUser).userDisplayName;
+}
