8847ace64e234a28f2e759abc947d57aacb4c056
Hasan Iqbal <swapniliqbal@gmail.com>
2021-07-05 22:36:28 -0400

    Minor updates to functions & css

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,33 +2,36 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
 
         // Display user name
-        updateDisplayNameInDOM()
+        updateDisplayNameInDOM(user)
 
         // Dropdown Nav Menu
         dropDownMenu()
 
         // Modal
-        updateProfileModal();
+        updateProfileModal(user);
 
         // Email Verification
         if (!user.emailVerified) {
-            // if user is unverified: show the option to verify and upon clicking it, verify the user.
+            // Show verify link
             document.querySelector("#verify-email-button").classList.toggle("show");
-            varifyEmail()
+
+            // Verify user
+            verifyEmail(user)
         } else {
             document.querySelector(".verified").classList.toggle("show")
         }
+
+        // Logout
+        document.getElementById('logout_btn').addEventListener("click", (e) => {
+            e.preventDefault();
+            auth.signOut();
+        })
 
     } else {
         // If the user is not logged in
         window.location.replace('index.html')
     }
 });
-
-// Database
-db.collection('guides').get().then(snapshot => {
-    console.log(snapshot.docs)
-})
 
 const dropDownMenu = () => {
     document.querySelector("#profile-dropdown").addEventListener("click", (e) => {
@@ -37,7 +40,7 @@ const dropDownMenu = () => {
     });
 }
 
-const updateProfileModal = () => {
+const updateProfileModal = (currentUser) => {
 
     // DOM Elements
     const modal = document.querySelector(".modal");
@@ -55,8 +58,8 @@ const updateProfileModal = () => {
         document.querySelector(".dropdown-content").classList.toggle("show");
 
         // Show user's email address and current username in placeholders
-        document.querySelector("#profile_email").placeholder = firebase.auth().currentUser.email;
-        document.querySelector("#profile_name").placeholder = firebase.auth().currentUser.displayName;
+        document.querySelector("#profile_email").placeholder = currentUser.email;
+        document.querySelector("#profile_name").placeholder = currentUser.displayName;
 
         // If the modal doesn't have the CSS "show" class:
         if (!modal.classList.contains("show")) {
@@ -76,7 +79,8 @@ const updateProfileModal = () => {
                     updateUsername(newName)
                     updateProfileForm.reset();
                     modal.classList.toggle("show")
-                } else if (newName == "") {
+                    firebase.auth().currentUser.reload();
+                } else {
                     window.alert("Must choose a new name to update")
                 }
             })
@@ -95,43 +99,37 @@ const updateUsername = (newName) => {
         displayName: newName
     }).then(() => {
         // Once the name has been updated, append it to the user dropdown menu
-        updateDisplayNameInDOM()
+        updateDisplayNameInDOM(firebase.auth().currentUser)
     });
 }
 
 // Verify Email Address
-const varifyEmail = () => {
+const verifyEmail = (currentUser) => {
     document.querySelector("#verify-email-button").addEventListener("click", (e) => {
         e.preventDefault();
 
         // Use Firebase's verification method to send the user a verification link
-        firebase.auth().currentUser.sendEmailVerification()
+        currentUser.sendEmailVerification()
             .then(() => {
                 // Hide the "Verify" button upon clicking it
                 document.querySelector("#verify-email-button").classList.toggle("show");
 
-                // Start a 1s listener to detect when user has verified their email address
+                // Start a 1 second listener to detect when user has verified their email address
                 this.emailVerificationListener = setInterval(() => {
-                    firebase.auth().currentUser.reload().then(ok => {
-                        // If the user verifies their email, kill the process and display the verification checkmark.
-                        if (firebase.auth().currentUser.emailVerified) {
-                            document.querySelector(".verified").classList.toggle("show")
-                            clearInterval(this.emailVerificationListener)
-                        }
-                    })
-                    // Once user clicks on "verify", this will listen for verification confirmation once every 2 seconds.
+                    currentUser.reload()
+                        .then(ok => {
+                            // If the user verifies their email, kill the process and display the verification checkmark.
+                            if (currentUser.emailVerified) {
+                                document.querySelector(".verified").classList.toggle("show");
+                                clearInterval(this.emailVerificationListener);
+                            }
+                        })
                 }, 1000)
             });
     })
 }
 
 // Show user's display name inside the DOM
-const updateDisplayNameInDOM = () => {
-    document.querySelector("#user_name").innerHTML = firebase.auth().currentUser.displayName;
+const updateDisplayNameInDOM = (currentUser) => {
+    document.querySelector("#user_name").innerHTML = currentUser.displayName;
 }
-
-// Logout
-document.getElementById('logout_btn').addEventListener("click", (e) => {
-    e.preventDefault();
-    auth.signOut();
-})
