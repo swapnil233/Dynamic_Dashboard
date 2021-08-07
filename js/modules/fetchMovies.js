2c746e8e9c892fddb80eeca26ffd344a7db916b7
Hasan Iqbal <swapniliqbal@gmail.com>
2021-08-07 13:02:47 -0400

    Implemented movies/shows searching feature
    Users can now search up their movies and TV shows from their dashboard. This is being done with Axios.js to handle HTTP requests instead of fetch, and the free Open Movie Database (OMDB) API.

---
+++ js/modules/fetchMovies.js
@@ -0,0 +1,40 @@
+document.querySelector(".search-button").addEventListener("click", e => {
+    e.preventDefault()
+
+    // Clear out the movies already showing
+    document.querySelector("#movies").innerHTML = '';
+
+    // Get movie search
+    let searchText = document.querySelector(".search-input").value;
+
+    getMovies(searchText);
+});
+
+const getMovies = (searchText) => {
+    axios.get('http://www.omdbapi.com?s='+ searchText + '&apikey=56bdb7b4').then((res) => {
+        let movies = res.data.Search;
+        console.table(res.data.Search)
+        
+        let output = ''
+
+        for (let i = 0; i<movies.length; i++) {
+            console.log(res.data.Search[i].Title)
+
+            output += `
+                <div class="movie-container-wrapper">
+                    <div class="movie-image-container">
+                        <img src=${movies[i].Poster}>
+                    </div>
+
+                    <div class="movie-info-container">
+                        <h5>${movies[i].Title}</h5>
+                    </div>
+                </div>
+            `
+        }
+
+        document.querySelector("#movies").innerHTML = output;
+    }).catch((err) => {
+        console.log(err)
+    })
+}
