368375cf56f39fe856b54e199ce52231aa3ba343
Hasan Iqbal <swapniliqbal@gmail.com>
2021-08-09 19:57:15 -0400

    Movies search result design improvements

--- js/modules/fetchMovies.js
+++ js/modules/fetchMovies.js
@@ -13,24 +13,28 @@ document.querySelector(".search-button").addEventListener("click", e => {
 const getMovies = (searchText) => {
     axios.get('https://www.omdbapi.com/?s='+ searchText + '&apikey=56bdb7b4').then((res) => {
         let movies = res.data.Search;
-        console.log(res.data.Search)
+        console.clear()
+        console.log(movies)
         
         let output = ''
 
         for (let i = 0; i<movies.length; i++) {
-            console.log(res.data.Search[i].Title)
+            console.log(movies[i].Title)
 
-            output += `
-                <div class="movie-container-wrapper">
-                    <div class="movie-image-container">
-                        <img src=${movies[i].Poster}>
+            if (movies[i].Type==="movie" || movies[i].Type=="series"){
+                output += 
+                `
+                <div class="movie-container">
+                    <div class="movie-image">
+                        <img src=${movies[i].Poster} alt="${movies[i].Title} Poster">
                     </div>
-
-                    <div class="movie-info-container">
-                        <h5>${movies[i].Title}</h5>
+                    <div class="movie-content">
+                        <h2 class="movie-name">${movies[i].Title}</h2>
+                        <p class="movie-release-date">Released: ${movies[i].Year}</p>
                     </div>
                 </div>
-            `
+                `
+            }
         }
 
         document.querySelector("#movies").innerHTML = output;
