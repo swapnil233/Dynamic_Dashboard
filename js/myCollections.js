2345efade1f782d0743051ced31c61c59e6d06cf
Hasan Iqbal <swapniliqbal@gmail.com>
2021-08-27 13:26:15 -0400

    Implemented Sorting Feature inside Collections Page
    Users can now sort which movies are displayed in their collections page by their collections. Improvements to come.

--- js/myCollections.js
+++ js/myCollections.js
@@ -4,6 +4,9 @@ import {
     errorPopup
 } from "./modules/interactions.js"
 
+// Global variable to store the movies_collections object
+var movies_collections_object;
+
 // While user is authenticated
 auth.onAuthStateChanged((user) => {
     if (user) {
@@ -16,7 +19,7 @@ auth.onAuthStateChanged((user) => {
             .get()
             .then((doc) => {
 
-                // If a "movies_collections" object doesn't exist, create it. This shouldn't ever be the case as it's created upon signup, but just in case.
+                // If a "movies_collections" object doesn't exist, create it.
                 if (!doc.data().movies_collections) {
                     userDocRef.set({
                         movies_collections: {}
@@ -29,12 +32,15 @@ auth.onAuthStateChanged((user) => {
                 // movies_collections ref
                 const movies_collections = doc.data().movies_collections;
 
+                // Global assignment of movies_collections object
+                movies_collections_object = movies_collections;
+
                 // For each movies_collections key
                 Object.keys(movies_collections).forEach(collection_name => {
 
                     // Add the name of each collection_name to the existing collections div
                     document.querySelector(".existing-collections").innerHTML +=
-                    `
+                        `
                     <div class="collection-name-container">
                         <p class="collection-name">
                             ${collection_name}
@@ -110,14 +116,16 @@ const createNewCollection = () => {
     })
 }
 
+// Create a new movies collection when the user clicks the "Create" button
 document.querySelector("#newCollectionBtn").addEventListener("click", createNewCollection);
-
 
 // Movie Insertion Helper Functions to add movies to the page sequentially instead of paralell
 const addMovieElementToContainerForID = (movie_imdbID) => {
     document.getElementById("movies-collections-container").innerHTML +=
         `<div class="movie-container" id="movie_${movie_imdbID}"></div>`;
 }
+
+// Update the movie element with the movie's data
 const updateMovieElement = (movie_imdbID, collection_name, res) => {
     let movieContainerElement = document.getElementById(`movie_${movie_imdbID}`);
     movieContainerElement.innerHTML =
@@ -139,3 +147,43 @@ const updateMovieElement = (movie_imdbID, collection_name, res) => {
         </div>
     `;
 }
+
+/*
+TODO: Ability to filter by collections
+
+1. Create a global variable 
+2. Assign the user's entire movies_collections object into that variable
+3. Create a function called filterMovies(movies_collections_object, filterBy)
+
+TODO: filterMovies function
+1. Go through the object's keys until you find the key that matches the filterBy input
+2. Return the object's value
+*/
+
+const filterMovies = (movies_collections_object, filterBy) => {
+    for (let key in movies_collections_object) {
+        if (key === filterBy) {
+            return movies_collections_object[key];
+        }
+    }
+}
+
+document.querySelector(".existing-collections").addEventListener("click", (event) => {
+    if (event.target.classList.contains("collection-name-container") || event.target.classList.contains("collection-name")) {
+        const collection_name = event.target.textContent.replace(/\s+/g, ' ').trim();
+        const collection = filterMovies(movies_collections_object, collection_name);
+        console.log(collection);
+        
+        // Empty the movies-collections-container div
+        document.getElementById("movies-collections-container").innerHTML = '';
+
+        // For each imdbID inside collection -> movies array, add the movie to the movies-collections-container div
+        collection.movies.forEach(movie => {
+            let movie_imdbID = movie.split("_")[0];
+            addMovieElementToContainerForID(movie_imdbID);
+            axios.get("https://www.omdbapi.com/?i=" + movie_imdbID + "&apikey=56bdb7b4").then((res) => {
+                updateMovieElement(movie_imdbID, collection_name, res)
+            })
+        })
+    }
+})
