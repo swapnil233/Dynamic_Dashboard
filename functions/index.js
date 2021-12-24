25473e80f6cae728c1f4c52fd624881c37428af9
Hasan Iqbal <swapniliqbal@gmail.com>
2021-12-23 21:28:08 -0500

    Updated firebase function for sending myself an email when a new user joins

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,40 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
 
+const admin = require("firebase-admin");
 admin.initializeApp();
+
+const nodemailer = require("nodemailer");
 
 require("dotenv").config();
 
+// From .env file
 const {
     SENDER_EMAIL,
-    SENDER_PASSWORD
+    SENDER_PASSWORD,
+    TO_EMAIL
 } = process.env;
 
+// smtp transporter
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: SENDER_EMAIL,
+        pass: SENDER_PASSWORD,
+    },
+});
+
 exports.sendEmailNotification = functions.firestore.document("users/{userId}").onCreate(async (snapshot, context) => {
-    const data = snapshot.data();
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: SENDER_EMAIL,
-            pass: SENDER_PASSWORD,
-        },
-    });
+    const mailOptions = {
+        from: `"FilmList.ca" <${SENDER_EMAIL}>`,
+        to: `${TO_EMAIL}`,
+        subject: `A New User Has Joined FilmList!`,
+        text: `A new user has joined FilmList. Name: ${snapshot.data().name}, email: ${snapshot.data().email}`
+    };
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"FilmList.ca" <tempiqbal233@gmail.com>',
-        to: "swapniliqbal@gmail.com",
-        subject: "New User!", // Subject line
-        text: `A new user has joined FilmList. Name: ${data.name}, email: ${data.email}`
-    });
+    try {
+        await mailTransport.sendMail(mailOptions);
+    } catch (error) {
+        functions.logger.error(error);
+    }
 })
